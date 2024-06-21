f52f9462a715ff8dcfb6b81c85847a688fc36ee8
michaelstrupler <michael.strupler@gmx.ch>
2024-06-21 12:06:39 +0200

    a

--- R/sample_PICARRO_data.R
+++ R/sample_PICARRO_data.R
@@ -5,11 +5,29 @@
 #' @docType data
 #' @name sample_PICARRO_data
 #' @usage data(sample_PICARRO_data)
-#' @format A data frame with X rows and Y variables:
+#' @format A data frame with 14393 rows and 18 variables:
 #' \describe{
-#'   \item{column1}{Description of column 1}
-#'   \item{column2}{Description of column 2}
+#'   \item{DATE}{Description of column 1}
+#'   \item{TIME}{Description of column 2}
+#'   \item{FRAC_DAYS_SINCE_JAN1}{Description of column 2}
+#'   \item{JULIAN_DAYS}{Description of column 2}
+#'   \item{EPOCH_TIME}{Description of column 2}
+#'   \item{ALARM_STATUS}{Description of column 2}
+#'   \item{INST_STATUS}{Description of column 2}
+#'   \item{solenoid_valves}{Description of column 2}
+#'   \item{N2O_dry1min}{Description of column 2}
+#'   \item{CO2_dry}{Description of column 2}
+#'   \item{CH4_dry}{Description of column 2}
+#'   \item{H2O}{Description of column 2}
+#'   \item{NH3}{Description of column 2}
+#'   \item{CHAMBER_TEMP_sync}{Description of column 2}
+#'   \item{SOIL_TEMP_sync}{Description of column 2}
+#'   \item{CHAMBER_PRESSURE_sync}{Description of column 2}
+#'   \item{SWITCH_sync}{Description of column 2}
+#'
 #'   ...
 #' }
-#' @source \url{http://source.of.data}
+
+"sample_PICARRO_data"
+
 
