fdfac4b4b7a2733bf48c0a2ce8747eb1800efbce
michaelstrupler <michael.strupler@gmx.ch>
2024-06-21 11:30:49 +0200

    sample data linked

---
+++ R/sample_PICARRO_data.R
@@ -0,0 +1,15 @@
+#' Sample_Picarro_data
+#'
+#' In this file, some sample Picarro Data is shown. Explain better later.
+#'
+#' @docType data
+#' @name sample_PICARRO_data
+#' @usage data(sample_PICARRO_data)
+#' @format A data frame with X rows and Y variables:
+#' \describe{
+#'   \item{column1}{Description of column 1}
+#'   \item{column2}{Description of column 2}
+#'   ...
+#' }
+#' @source \url{http://source.of.data}
+
